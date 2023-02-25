c6498beda46291bc929d86bf7ceab9900a2ecea2
Juan José Rodríguez Duque <jjrd91@hotmail.com>
2023-02-25 10:58:08 -0500

    first commit

---
+++ src/test/java/testingBooks/ApiTests.java
@@ -0,0 +1,12 @@
+package testingBooks;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ApiTests {
+
+    @Test
+    public void getStatus() {
+        String endpoint = "https://simple-books-api.glitch.me/status";
+    }
+}
