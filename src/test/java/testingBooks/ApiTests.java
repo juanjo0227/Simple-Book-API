6bd068c6d33e9b5e7575a30555bd1e34fe6e08b5
Juan José Rodríguez Duque <jjrd91@hotmail.com>
2023-02-25 19:32:21 -0500

    first two tests

--- src/test/java/testingBooks/ApiTests.java
+++ src/test/java/testingBooks/ApiTests.java
@@ -3,10 +3,33 @@ package testingBooks;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class ApiTests {
 
     @Test
     public void getStatus() {
         String endpoint = "https://simple-books-api.glitch.me/status";
+        given().
+                when().
+                    get(endpoint).
+                then().
+                    assertThat().statusCode(200).
+                    assertThat().body("status", equalTo("OK"));
+    }
+
+    @Test
+    public void getListOfBooks() {
+        String endpoint = "https://simple-books-api.glitch.me/books";
+        given().
+                queryParam("type", "non-fiction").
+        when().
+                get(endpoint).
+        then().
+                assertThat().statusCode(200).
+                body("book.size()", greaterThan(0)).
+                body("book.id", everyItem(notNullValue())).
+        log().body();
     }
 }
